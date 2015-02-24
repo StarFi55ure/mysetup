69113f536be250c219eee93a4eecd865be99ac09
Julian Kennedy <juliankenn@gmail.com>
2015-02-24 17:46:11 +0200

    - some cleanups of the aliases

---
+++ zshsetup/global_aliases.zsh
@@ -0,0 +1,10 @@
+alias vim="gvim"
+alias cvim="/usr/bin/vim"
+
+alias ems='cd $ZSH_SETUP_ROOT'
+alias emv='cd $HOME/.myvim'
+
+alias g='git'
+alias ll="ls --color=auto -Flash"
+
+alias nicedate="date +'%Y-%m-%d'"
