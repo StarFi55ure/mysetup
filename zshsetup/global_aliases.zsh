a4038af56a970a8998046ac07e6935ae5a01a5f2
Julian Kennedy <juliankenn@gmail.com>
2018-10-03 12:07:46 +0200

    - added some aliases

--- zshsetup/global_aliases.zsh
+++ zshsetup/global_aliases.zsh
@@ -22,3 +22,13 @@ alias ve3="virtualenv -p python3 pyenv"
 alias ve2="virtualenv -p python pyenv"
 alias pact="source pyenv/bin/activate"
 
+alias untar="tar -xvf "
+alias wget="wget -c "
+alias getpass="openssl rand -base64 20"
+alias sha="shasum -a 256 "
+alias ping="ping -c 5"
+
+alias www="python -m SimpleHTTPServer 7000"
+alias ipe="curl ipinfo.io/ip"
+alias ipa="ip addr"
+
