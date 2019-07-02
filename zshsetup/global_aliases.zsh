6537485e4fdb187153a6f5d8a4ff10e61088e489
Julian Kennedy <juliankenn@gmail.com>
2019-07-02 19:19:14 +0200

    added maven alias

--- zshsetup/global_aliases.zsh
+++ zshsetup/global_aliases.zsh
@@ -2,6 +2,8 @@
 alias ems='cd $ZSH_SETUP_ROOT'
 alias emv='cd $HOME/.myvim'
 
+# git
+
 alias g='git'
 alias gp='git push'
 alias gpt='git push --tags'
@@ -9,20 +11,23 @@ alias gpt='git push --tags'
 alias ll="ls --color=auto -Flsh"
 
 alias hashr="hash -r"
-
 alias nicedate="date +'%Y-%m-%d'"
 
 alias sctl="supervisorctl"
 
+# editor
+#
 alias v="vim"
 alias gv="gvim"
 
 alias ta="tmux attach"
 
+# environments
 alias ve3="virtualenv -p python3 pyenv"
 alias ve2="virtualenv -p python pyenv"
 alias pact="source pyenv/bin/activate"
 
+alias mvn11="JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 mvn"
 alias untar="tar -xvf "
 alias wget="wget -c "
 alias getpass="openssl rand -base64 20"
@@ -33,3 +38,6 @@ alias www="python -m SimpleHTTPServer 7000"
 alias ipe="curl ipinfo.io/ip"
 alias ipa="ip addr"
 
+# Development
+#
+alias postpsql="sudo -u postgres psql"
